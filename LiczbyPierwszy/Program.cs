5d60d6f40c97f43f4e90d0b9f3e8ebb9b60c5dc5
Matthew <mateusz.kajzer2807pl@gmail.com>
2022-05-24 14:08:42 +0200

    Add files via upload

---
+++ LiczbyPierwszy/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+
+        zadanie1();
+        foregroundColor();
+        zadanie2();
+
+    }
+    static void foregroundColor()
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+    }
+    static void zadanie1()
+    {
+        Console.WriteLine("Wprowadz temp w st. Celsjusza");
+        int degrees = Convert.ToInt32(Console.ReadLine());
+
+        double fahrenheitDeg = (degrees * 1.8) + 32;
+
+        Console.WriteLine($"Temperatura w stopnich Fahreheita to {fahrenheitDeg}");
+    }
+
+    static void zadanie2()
+    {
+        Console.WriteLine("WProwadz 3 liczby");
+        long liczba1 = Convert.ToInt64(Console.ReadLine());
+        long liczba2 = Convert.ToInt64(Console.ReadLine());
+        long liczba3 = Convert.ToInt64(Console.ReadLine());
+
+        Console.WriteLine($"Najwieksza liczaba to: {(Math.Max(Math.Max(liczba1, liczba2), liczba3))}");
+        Console.WriteLine($"Najmniejsza liczba to: {(Math.Min(Math.Min(liczba1, liczba2), liczba3))}");
+    }
+}
---
+++ LiczbyPierwszy/ProgramHelpers.cs
@@ -0,0 +1,20 @@
+﻿internal static class ProgramHelpers
+{
+    public static string Main()
+    {
+
+        string number = GetNumber("dsf");
+        Console.WriteLine("Witaj w pogramie! Wprowadz liczbe aby sprawdzic czy jest liczbą pierwszą");
+
+        string inputNumber = Console.ReadLine();
+        return inputNumber;
+    }
+
+    static string GetNumber(string number)
+    {
+        Console.WriteLine("Witaj w pogramie! Wprowadz liczbe aby sprawdzic czy jest liczbą pierwszą");
+
+        string inputNumber = Console.ReadLine();
+        return inputNumber;
+    }
+}
